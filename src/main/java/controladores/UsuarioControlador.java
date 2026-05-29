65e3426a6109bc8561384d6de4c3179fcfec0d87
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-05-29 10:50:07 -0300

    feat: controlador de usuário busca por cpf, cadastro de usuario (não permite cadastro com cpf já existente) e autenticação de usuário

--- src/main/java/controladores/UsuarioControlador.java
+++ src/main/java/controladores/UsuarioControlador.java
@@ -1,4 +1,59 @@
 package controladores;
 
+import br.edu.ifpar.tsi3.tsfy.dominio.Usuario;
+import java.util.ArrayList;
+
 public class UsuarioControlador {
+
+    private ArrayList<Usuario> usuarios;
+
+    public UsuarioControlador() {
+        this.usuarios = new ArrayList<>();
+    }
+
+    public Usuario buscarUsuarioPorCpf(String cpf){
+
+        for(Usuario u : this.usuarios){
+
+            if(u.getCpf().equals(cpf)){
+                return u;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean cadastrarUsuario(String cpf, String nome, String senha){
+
+        if(buscarUsuarioPorCpf(cpf) != null){
+            return false; 
+        }
+
+        if (cpf.equals(senha)) {
+            return false; 
+        }
+
+        Usuario novoUsuario = new Usuario(cpf, nome, senha);
+
+        this.usuarios.add(novoUsuario);
+
+        return true;
+    }
+
+   
+
+    public boolean autenticarUsuario(String cpf, String senha){
+        
+        Usuario usuario =buscarUsuarioPorCpf(cpf);
+
+        if(usuario == null){
+            return false;
+        }
+
+        return usuario.getSenha().equals(senha);
+    }
+
+    public ArrayList<Usuario> listarUsuarios(){
+        return this.usuarios;
+    }
 }
